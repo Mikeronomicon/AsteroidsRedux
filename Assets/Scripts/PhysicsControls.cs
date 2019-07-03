88882a290c2d7aaf6486d24411f26b99237be4c8
Michael Lorenzo <mikeronomicon@gmail.com>
2019-07-03 13:08:43 -0700

    Removed Unity Demo asset files
    Removed demo assets and updated character controller, now with 100% more movement.

--- Assets/Scripts/PhysicsControls.cs
+++ Assets/Scripts/PhysicsControls.cs
@@ -6,6 +6,7 @@ public class PhysicsControls : MonoBehaviour
 {
 
     private Transform ThisTransform = null;
+    private Rigidbody ShipBody = null;
     public float Speed = 1f;
     public string HorzAxis = "Horizontal";
     public string VertAxis = "Vertical";
@@ -14,6 +15,19 @@ public class PhysicsControls : MonoBehaviour
 
     {
         ThisTransform = GetComponent<Transform>();
+        ShipBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+
+    {
+        float Horz = Input.GetAxis(HorzAxis);
+        float Vert = Input.GetAxis(VertAxis);
+
+        Vector3 HorzVelocity = ThisTransform.right * Horz * Speed;
+        Vector3 VertVelocity = ThisTransform.forward * Vert * Speed;
+
+        ShipBody.velocity = HorzVelocity + VertVelocity;
     }
 
 }
