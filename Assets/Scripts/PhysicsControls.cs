def3e866bc10493c91e972f784ba4fcabb2bce36
Michael Lorenzo <mikeronomicon@gmail.com>
2019-07-03 11:42:00 -0700

    First Commit
    Setting up Repo and project files.

---
+++ Assets/Scripts/PhysicsControls.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsControls : MonoBehaviour
+{
+
+    private Transform ThisTransform = null;
+    public float Speed = 1f;
+    public string HorzAxis = "Horizontal";
+    public string VertAxis = "Vertical";
+
+    private void Awake()
+
+    {
+        ThisTransform = GetComponent<Transform>();
+    }
+
+}
