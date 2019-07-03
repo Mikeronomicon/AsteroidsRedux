ed84a4676b8d91815aeeabbd067b3ec49f5950e2
Michael Lorenzo <mikeronomicon@gmail.com>
2019-07-03 13:35:05 -0700

    Added Health Script
    Finished a working script to handle player health and death. I think it can be a whole lot better though.

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    public float HealthPoints = 100f;
+
+    private void Update()
+    {
+        if(HealthPoints <= 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
